3649f8c92565ead1db6951fac3b9ede3fb6c1be3
TalyaHakim <talyahakim63@icloud.com>
2020-12-31 23:39:33 +0200

    Add Starter Code

---
+++ tictactoe/src/Components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Square extends React.Component{
+    render() {
+        return (
+          <button className="square">
+            {/* TODO */}
+          </button>
+        );
+      }
+}
+export default Square;  
